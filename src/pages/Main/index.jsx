99d617b4b65c89e5c9064a1f73d3254f03e66c3f
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-04-26 20:20:29 -0300

    feat: added quagga

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import { Container } from "./styles";
+import Quagga from "quagga";
+import { Video } from "./styles";
+
 
 export default function Main() {
+    
+    useEffect(() => {
+      if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        Quagga.init({
+          inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector("#video"),
+            constraits: {
+              facingMode: "environment",
+            },
+          },
+          numOfWorkers: 1,
+          locate: true,
+          decoder: {
+            readers: ["ean_reader"],
+          }
+        },
+      err => {
+        if(err) {
+          console.error(err);
+          alert("Erro ao abrir a câmera dos dispositivo");
+          return;
+        }
+
+        Quagga.start();
+      }
+      );
+      }
+    }, []);
+
   return (
-    <Container>
-      PWA-SCANNER
-    </Container>
-  );
+    <Video id="video" />
+
+);
 }
